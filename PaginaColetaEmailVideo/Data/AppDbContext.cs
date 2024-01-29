741d26c46af5a5189d6c759f93e8e7e96a4b6dc6
Crislaine <clpaula3007@gmail.com>
2024-01-29 10:12:50 -0300

    Adicionar arquivos de projeto.

---
+++ PaginaColetaEmailVideo/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaginaColetaEmailVideo.Models;
+
+namespace PaginaColetaEmailVideo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmailModel> Emails { get; set; }
+    }
+}
